0345599bb075cda1e571495fa58ca8067adfdcf7
wuxu <wuxu@hengan.com>
2020-09-12 11:57:59 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [require.resolve('@umijs/fabric/dist/eslint')],
+    globals: {
+        ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+        page: true,
+        REACT_APP_ENV: true,
+    },
+    rules: {
+        // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-console": "warn",
+        "no-debugger": "error",
+        // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-alert": 2,
+
+        // "no-unused-vars": "warn",
+        // "no-tabs": "error",
+        // "indent": ["error", 4],
+        // "comma-dangle": [
+        //     "warn",
+        //     {
+        //         arrays: "always-multiline",
+        //         objects: "always-multiline",
+        //         imports: "never",
+        //         exports: "never",
+        //         functions: "ignore",
+        //     },
+        // ],
+        // "no-var": "warn",
+    },
+};
